559de562ccf7962f80f04d44b735f6d20b6d7165
Saniha501 <sanihagowda25@gmail.com>
2024-10-18 15:09:11 +0530

    Pushing

---
+++ Jenkins/src/test/java/JenkinsDemo/JenkinsPratice.java
@@ -0,0 +1,13 @@
+package JenkinsDemo;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPratice {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("Hello Sani");
+	}
+
+}
---
+++ Jenkins/src/test/java/JenkinsDemo/JenkinsTest.java
@@ -0,0 +1,13 @@
+package JenkinsDemo;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	@Test
+	public void jen()
+	{
+		System.out.println("Jenkins is proccessing");
+	}
+
+}
